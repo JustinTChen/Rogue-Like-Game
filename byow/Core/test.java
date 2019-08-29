05d1be0a5410c1da40a36b5f9442a89ab52fa62f
Justin Chen <jchen23@berkeley.edu>
2019-08-29 11:16:48 -0700

    Original Version updated

---
+++ byow/Core/test.java
@@ -0,0 +1,10 @@
+package byow.Core;
+
+import byow.Core.Engine;
+public class test {
+
+    public static void main(String[] args){
+        Engine play = new Engine();
+        play.interactWithKeyboard();;
+    }
+}
